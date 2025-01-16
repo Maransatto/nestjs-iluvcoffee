86253f0c9ca017358e2095f391a3f5cf986f07a7
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-16 20:44:46 -0300

    feat: Introducing the TypeORM Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const ORM_OPTIONS: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  autoLoadEntities: true,
+  synchronize: true,
+};
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot(ORM_OPTIONS)],
   controllers: [AppController],
   providers: [AppService],
 })
