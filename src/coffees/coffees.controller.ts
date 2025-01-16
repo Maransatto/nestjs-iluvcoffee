dbde51f6e3cef0896caaad828ade2b3c6519c1bf
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-16 13:35:01 -0300

    feat: Handling Request Body / Payload

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -11,5 +11,11 @@ export class CoffeesController {
     @Get(':id')
     findOne(@Param('id') id: string) {
         return `This actions returns #${id} coffee`
+    }
+
+    @Post()
+    create(@Body() body) {
+        return body;
+        // return 'This action creates a coffee'
     }
 }
