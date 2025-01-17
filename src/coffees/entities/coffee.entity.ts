5fa6a664aecd41ccb3909aebc502a898c124fbbf
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-16 21:03:15 -0300

    feat: Creating a TypeORM Entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('coffees') // sql table === 'coffee' if not specified in the decorator
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
