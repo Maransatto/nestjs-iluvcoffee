573b67c8b617cd14a93082b741ec01dc60af0ecd
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-18 12:24:58 -0300

    feat: Introducing the Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesModule } from './coffees/coffees.module';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { CoffeesModule } from './coffees/coffees.module';
 import { DatabaseModule } from './database/database.module';
 
-const ORM_OPTIONS: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'pass123',
-  database: 'postgres',
-  autoLoadEntities: true,
-  synchronize: true,
-};
-
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot(ORM_OPTIONS), CoffeeRatingModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoffeeRatingModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
