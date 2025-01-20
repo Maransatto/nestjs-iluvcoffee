5405ed7039773b0b3c7f7a7dc5d2e0cbb5ab1510
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-20 16:53:10 -0300

    feat: Protect Routes with Guards

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +15,8 @@ async function bootstrap() {
       },
     }),
   );
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
